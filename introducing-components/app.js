6644605bffa6264c2c3278146914579afb7a666c
unknown <miracledev0303@gmail.com>
2022-11-28 01:33:13 -0400

    understanding components

--- introducing-components/app.js
+++ introducing-components/app.js
@@ -15,7 +15,32 @@ const app = Vue.createApp({
           email: "julie@localhost.com",
         },
       ],
+    };
+  },
+});
+
+app.component("friend-contact", {
+  template: `
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">
+            {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+        </button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+  `,
+  data() {
+    return {
       detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "01234 5678 991",
+        email: "manuel@localhost.com",
+      }
     };
   },
   methods: {
