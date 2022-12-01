75b982225b04b48437d808bbb99658f9cc932215
unknown <miracledev0303@gmail.com>
2022-12-01 01:33:29 -0400

    emitting custom events for communication from child to parent

--- vue-first-app/src/components/FriendContact.vue
+++ vue-first-app/src/components/FriendContact.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <h2>{{ name }} {{ friendIsFavorite ? '(Favorite)' : '' }}</h2>
+    <h2>{{ name }} {{ isFavorite ? '(Favorite)' : '' }}</h2>
     <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
@@ -25,6 +25,10 @@ export default {
   //   'isFavorite'
   // ],
   props: {
+    id: {
+      type: String,
+      required: true
+    },
     name: {
       type: String,
       required: true,
@@ -45,8 +49,7 @@ export default {
   },
   data() {
     return {
-      detailsAreVisible: false,
-      friendIsFavorite: this.isFavorite
+      detailsAreVisible: false
     };
   },
   methods: {
@@ -54,7 +57,7 @@ export default {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
     toggleFavorite() {
-      this.friendIsFavorite = !this.friendIsFavorite;
+      this.$emit('toggle-favorite', this.id);
     }
   }
 };
