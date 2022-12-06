de29dbd1fd3aa3d465ddae180e9d9aaf2004b1e4
unknown <miracledev0303@gmail.com>
2022-12-06 01:57:03 -0400

    provide + inject for functions

--- component-communication-problem/src/App.vue
+++ component-communication-problem/src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <active-element :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"></active-element>
-    <knowledge-base @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base></knowledge-base>
   </div>
 </template>
 
@@ -32,7 +32,8 @@ export default {
   },
   provide() {
     return {
-      topics: this.topics
+      topics: this.topics,
+      selectTopic: this.activateTopic
     };
   },
   methods: {
