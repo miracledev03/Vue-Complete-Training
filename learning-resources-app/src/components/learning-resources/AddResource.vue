2995b7a9f12c8ef229bf2dfcc1518410056bb390
unknown <miracledev0303@gmail.com>
2022-12-16 01:52:53 -0400

    adding a form

--- learning-resources-app/src/components/learning-resources/AddResource.vue
+++ learning-resources-app/src/components/learning-resources/AddResource.vue
@@ -1,5 +1,23 @@
 <template>
-    <h2>Add Resource</h2>
+    <base-card>
+        <form>
+            <div class="form-control">
+                <label for="title">Title</label>
+                <input id="title" name="title" type="text">
+            </div>
+            <div class="form-control">
+                <label for="description">Title</label>
+                <textarea id="description" name="description" rows="3"></textarea>
+            </div>
+            <div class="form-control">
+                <label for="link">Link</label>
+                <input id="link" name="link" type="url">
+            </div>
+            <div>
+                <base-button type="submit">Add Resource</base-button>
+            </div>
+        </form>
+    </base-card>
 </template>
 
 <script>
