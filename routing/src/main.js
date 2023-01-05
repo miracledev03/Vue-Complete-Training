220ea2f6a27cd121d9f70307bb734109a6d61881
unknown <miracledev0303@gmail.com>
2023-01-05 17:59:59 -0400

    nested routes

--- routing/src/main.js
+++ routing/src/main.js
@@ -1,27 +1,38 @@
-import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-import App from './App.vue';
-import TeamsList from './components/teams/TeamsList.vue';
-import UsersList from './components/users/UsersList.vue';
-import TeamMembers from './components/teams/TeamMembers.vue';
-import NotFound from './components/nav/NotFound.vue';
+import App from "./App.vue";
+import TeamsList from "./components/teams/TeamsList.vue";
+import UsersList from "./components/users/UsersList.vue";
+import TeamMembers from "./components/teams/TeamMembers.vue";
+import NotFound from "./components/nav/NotFound.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/', redirect: '/teams' },
-        // { path: '/teams', component: TeamsList, alias: '/' },
-        { path: '/teams', component: TeamsList },   // our-domain.com/teams => ...
-        { path: '/users', component: UsersList },
-        { path: '/teams/:teamId', component: TeamMembers, props: true },
-        { path: '/:notFound(.*)', component: NotFound }
-    ],
-    linkActiveClass: 'active',
+  history: createWebHistory(),
+  routes: [
+    { path: "/", redirect: "/teams" },
+    // { path: '/teams', component: TeamsList, alias: '/' },
+    {
+      name: "teams",
+      path: "/teams",
+      component: TeamsList,
+      children: [
+        {
+          name: "team-members",
+          path: ":teamId",
+          component: TeamMembers,
+          props: true,
+        },
+      ],
+    }, // our-domain.com/teams => ...
+    { path: "/users", component: UsersList },
+    { path: "/:notFound(.*)", component: NotFound },
+  ],
+  linkActiveClass: "active",
 });
 
-const app = createApp(App)
+const app = createApp(App);
 
 app.use(router);
 
-app.mount('#app');
+app.mount("#app");
