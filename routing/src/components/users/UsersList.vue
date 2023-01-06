38e63010e645e5ac3527eb10ddc200b2ee2f2b5c
unknown <miracledev0303@gmail.com>
2023-01-06 16:59:41 -0400

    navigation guards deeper insight

--- routing/src/components/users/UsersList.vue
+++ routing/src/components/users/UsersList.vue
@@ -18,6 +18,11 @@ export default {
       // do something
       this.$router.push('/teams');
     }
+  },
+  beforeRouteEnter(to, from, next) {
+    console.log('UsersList component beforeRouteEnter');
+    console.log(to, from);
+    next();
   }
 };
 </script>
