c16b77aaa63374446d23af2399f215fbaa8958bf
unknown <miracledev0303@gmail.com>
2023-01-11 10:47:14 -0400

    using mapper helpers

--- vuex/src/components/ChangeCounter.vue
+++ vuex/src/components/ChangeCounter.vue
@@ -1,13 +1,21 @@
 <template>
-    <button @click="addOne">Add 1</button>
+    <button @click="inc">Add 1</button>
+    <button @click="increase({ value: 11 })">Add 11</button>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
     methods: {
-        addOne() {
-            this.$store.commit('increment');
-        }
-    }
+        // addOne() {
+        //     this.$store.commit('increment');
+        // }
+        // ...mapActions(['increment', 'increase'])
+        ...mapActions({
+            inc: 'increment',
+            increase: 'increase',
+        })
+    },
 }
 </script>
