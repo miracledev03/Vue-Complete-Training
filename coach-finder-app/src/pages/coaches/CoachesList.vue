710bde4d9dd2885454d5e355be73c499b24311bd
unknown <miracledev0303@gmail.com>
2023-01-18 14:22:03 -0400

    filtering coaches

--- coach-finder-app/src/pages/coaches/CoachesList.vue
+++ coach-finder-app/src/pages/coaches/CoachesList.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-        FILTER
+        <coach-filter @change-filter="setFilters"></coach-filter>
     </section>
     <section>
         <base-card>
@@ -20,17 +20,51 @@
 
 <script>
 import CoachItem from '@/components/coaches/CoachItem.vue';
+import CoachFilter from '@/components/coaches/CoachFilter.vue';
 
 export default {
     components: {
-        CoachItem
+        CoachItem,
+        CoachFilter
+    },
+    data() {
+        return {
+            activeFilters: {
+                frontend: true,
+                backend: true,
+                career: true,
+            }
+        }
     },
     computed: {
         filteredCoaches() {
-            return this.$store.getters['coaches/coaches'];
+            const coaches = this.$store.getters['coaches/coaches'];
+            return coaches.filter(coach => {
+                if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+                    console.log('frontend');
+                    return true;
+                }
+
+                if (this.activeFilters.backend && coach.areas.includes('backend')) {
+                    console.log('backend');
+                    return true;
+                }
+
+                if (this.activeFilters.career && coach.areas.includes('career')) {
+                    console.log('career');
+                    return true;
+                }
+
+                return false;
+            });
         },
         hasCoaches() {
             return this.$store.getters['coaches/hasCoaches'];
+        }
+    },
+    methods: {
+        setFilters(updatedFilters) {
+            this.activeFilters = updatedFilters;
         }
     }
 }
