c0ca819149ccd77c71c4792e5c7580cd5ad60820
unknown <miracledev0303@gmail.com>
2023-01-19 10:55:45 -0400

    sending a put http requests to store coach data

--- coach-finder-app/src/store/modules/coaches/actions.js
+++ coach-finder-app/src/store/modules/coaches/actions.js
@@ -1,14 +1,31 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
-      description: data.description,
+      description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas,
     };
 
-    context.commit("registerCoach", coachData);
+    const response = await fetch(
+      `https://coach-finder-c7e91-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: "PUT",
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // error ...
+    }
+
+    context.commit("registerCoach", {
+      ...coachData,
+      id: userId,
+    });
   },
 };
