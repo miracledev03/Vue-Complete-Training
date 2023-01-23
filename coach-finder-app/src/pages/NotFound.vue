d0796a73d8a4e240334c67897778a60165aec089
unknown <miracledev0303@gmail.com>
2023-01-23 14:58:32 -0400

    the 'Not Found' page

--- coach-finder-app/src/pages/NotFound.vue
+++ coach-finder-app/src/pages/NotFound.vue
@@ -1,3 +1,8 @@
 <template>
-    <section></section>
+    <section>
+        <base-card>
+            <h2>Page not found</h2>
+            <p>This page could not be found - maybe check out all our <router-link to="/coaches">Coaches</router-link></p>
+        </base-card>
+    </section>
 </template>
