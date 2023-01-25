61ea79142d2ff2af279b6bff90a1ac77b6bd97a3
unknown <miracledev0303@gmail.com>
2023-01-25 15:58:01 -0400

    loading spinner and error handling

--- coach-finder-app/src/pages/auth/UserAuth.vue
+++ coach-finder-app/src/pages/auth/UserAuth.vue
@@ -1,19 +1,29 @@
 <template>
-    <base-card>
-        <form @submit.prevent="submitForm">
-            <div class="form-control">
-                <label for="email">E-Mail</label>
-                <input type="email" id="email" v-model="email">
-            </div>
-            <div class="form-control">
-                <label for="password">Password</label>
-                <input type="password" id="password" v-model="password">
-            </div>
-            <p v-if="!formIsValid">Please enter a valid email and password (must be at 6 characters long.)</p>
-            <base-button>{{ submitButtonCaption }}</base-button>
-            <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeButtonCaption }}</base-button>
-        </form>
-    </base-card>
+    <div>
+        <base-dialog :show="!!error" title="An error occured" @close="handleError">
+            <p>{{ error }}</p>
+        </base-dialog>
+        <base-dialog :show="isLoading" title="Authenticating..." fixed>
+            <base-spinner></base-spinner>
+        </base-dialog>
+        <base-card>
+            <form @submit.prevent="submitForm">
+                <div class="form-control">
+                    <label for="email">E-Mail</label>
+                    <input type="email" id="email" v-model="email">
+                </div>
+                <div class="form-control">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" v-model="password">
+                </div>
+                <p v-if="!formIsValid">Please enter a valid email and password (must be at 6 characters long.)</p>
+                <base-button>{{ submitButtonCaption }}</base-button>
+                <base-button type="button" mode="flat" @click="switchAuthMode">{{
+                    switchModeButtonCaption
+                }}</base-button>
+            </form>
+        </base-card>
+    </div>
 </template>
 
 <script>
@@ -23,7 +33,9 @@ export default {
             email: '',
             password: '',
             formIsValid: true,
-            mode: 'login'
+            mode: 'login',
+            isLoading: false,
+            error: null,
         };
     },
     computed: {
@@ -43,20 +55,28 @@ export default {
         },
     },
     methods: {
-        submitForm() {
+        async submitForm() {
             if (this.email === '' || !this.email.includes('@') || this.password.length < 6) {
                 this.formIsValid = false;
                 return;
             }
 
-            if (this.mode === 'login') {
-                // ...
-            } else {
-                this.$store.dispatch('signup', {
-                    email: this.email,
-                    password: this.password
-                });
+            this.isLoading = true;
+
+            try {
+                if (this.mode === 'login') {
+                    // ...
+                } else {
+                    await this.$store.dispatch('signup', {
+                        email: this.email,
+                        password: this.password
+                    });
+                }
+            } catch (err) {
+                this.error = err.message || 'Failed to authenticate. Check your login data.';
             }
+
+            this.isLoading = false;
         },
         switchAuthMode() {
             if (this.mode === 'login') {
@@ -64,6 +84,9 @@ export default {
             } else {
                 this.mode = 'login';
             }
+        },
+        handleError() {
+            this.error = null;
         },
     }
 }
