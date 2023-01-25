4e5d8ff15edf79a2504ebf63d992876465ac0968
unknown <miracledev0303@gmail.com>
2023-01-25 16:05:27 -0400

    attaching the token to outgoing requests

--- coach-finder-app/src/store/modules/coaches/actions.js
+++ coach-finder-app/src/store/modules/coaches/actions.js
@@ -9,8 +9,10 @@ export default {
       areas: data.areas,
     };
 
+    const token = context.rootGetters.token;
+
     const response = await fetch(
-      `https://coach-finder-c7e91-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      `https://coach-finder-c7e91-default-rtdb.firebaseio.com/coaches/${userId}.json?auth=${token}`,
       {
         method: "PUT",
         body: JSON.stringify(coachData),
