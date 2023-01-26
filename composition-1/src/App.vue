4a052e032991d83fc0db8051db8039b00958c52a
unknown <miracledev0303@gmail.com>
2023-01-26 16:11:24 -0400

    replacing computed properties with the computed function

--- composition-1/src/App.vue
+++ composition-1/src/App.vue
@@ -1,26 +1,45 @@
 <template>
   <section class="container">
-    <h2>{{ user.name }}</h2>
+    <h2>{{ userName }}</h2>
     <h3>{{ user.age }}</h3>
     <button @click="setAge">Change Age</button>
+    <div>
+      <input type="text" placeholder="First Name" @input="setFirstName" />
+      <input type="text" placeholder="Last Name" @input="setLastName" />
+    </div>
   </section>
 </template>
 
 <script>
-import { reactive } from 'vue';
+import { ref, reactive, computed } from 'vue';
 
 export default {
   setup() {
     // const uName = ref('Maximilian');
     // const uAge = ref(31);
 
+    const firstName = ref('');
+    const lastName = ref('');
+
     const user = reactive({
       name: 'Maximilian',
       age: 31
     });
 
+    const uName = computed(function () {
+      return firstName.value + ' ' + lastName.value;
+    });
+
     function setNewAge() {
       user.age = 33;
+    }
+
+    function setFirstName(event) {
+      firstName.value = event.target.value;
+    }
+
+    function setLastName(event) {
+      lastName.value = event.target.value;
     }
 
     // console.log(isRef(uAge));
@@ -42,8 +61,11 @@ export default {
     return {
       // userName: user.name,
       // age: user.age,
+      userName: uName,
       user: user,
       setAge: setNewAge,
+      setFirstName,
+      setLastName,
     };
   },
   // data() {
