3177f7a3c228ae8248a0ab195b079a6d8aca2365
unknown <miracledev0303@gmail.com>
2023-01-27 17:36:05 -0400

    Route, Router

--- composition-routing/src/pages/ProductDetails.vue
+++ composition-routing/src/pages/ProductDetails.vue
@@ -9,17 +9,20 @@
 
 <script>
 import { inject, computed } from 'vue';
+import { useRoute } from 'vue-router';
 
 export default {
   props: ['pid'],
-  setup(props) {
+  setup() {
     const products = inject('products');
+
+    const route = useRoute();
 
     // const title = ref('');
     // const price = ref(null);
     // const description = ref('');
 
-    const selectedProduct = computed(() => products.value.find(product => product.id === props.pid));
+    const selectedProduct = computed(() => products.value.find(product => product.id === route.params.pid));
     const title = computed(() => selectedProduct.value.title);
     const price = computed(() => selectedProduct.value.price);
     const description = computed(() => selectedProduct.value.description);
