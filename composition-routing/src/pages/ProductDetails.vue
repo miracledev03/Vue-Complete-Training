c3d24eb5adf96f652c44bca764609182133f5a7d
unknown <miracledev0303@gmail.com>
2023-01-27 17:26:44 -0400

    routing

--- composition-routing/src/pages/ProductDetails.vue
+++ composition-routing/src/pages/ProductDetails.vue
@@ -3,17 +3,26 @@
     <h2>{{ title }}</h2>
     <h3>${{ price}}</h3>
     <p>{{ description}}</p>
+    <router-link to="/products/p2">Product 2</router-link>
   </section>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { inject, computed } from 'vue';
 
 export default {
-  setup() {
-    const title = ref('');
-    const price = ref(null);
-    const description = ref('');
+  props: ['pid'],
+  setup(props) {
+    const products = inject('products');
+
+    // const title = ref('');
+    // const price = ref(null);
+    // const description = ref('');
+
+    const selectedProduct = computed(() => products.value.find(product => product.id === props.pid));
+    const title = computed(() => selectedProduct.value.title);
+    const price = computed(() => selectedProduct.value.price);
+    const description = computed(() => selectedProduct.value.description);
 
     return { title, price, description };
   },
